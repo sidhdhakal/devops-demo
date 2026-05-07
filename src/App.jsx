ec60f672c7e7e201f0aebecd68becf75e6239d2c
sidhdhakal <siddharthadhakal54@gmail.com>
2026-05-07 16:55:20 +0545

    pthon

--- src/App.jsx
+++ src/App.jsx
@@ -217,7 +217,7 @@ export default function App() {
             marginBottom: 16,
           }}
         >
-          Welcome to <span style={{ color: "#378ADD" }}>Python</span>
+          Welcome to <span style={{ color: "#378ADD" }}>QA</span>
           <br />
           Ship faster. Break nothing.
         </h1>
